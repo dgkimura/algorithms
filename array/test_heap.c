366f0c5fef88a058505d21378732250333a1b6fb
David Kimura <dkimura@pivotal.io>
2019-05-11 23:33:41 -0700

    Add testcase for multiple ascending inserts

--- array/test_heap.c
+++ array/test_heap.c
@@ -53,6 +53,22 @@ START_TEST(test_heap_with_many_decreasing_values)
 }
 END_TEST
 
+START_TEST(test_heap_with_many_increasing_values)
+{
+    struct heap h;
+    heap_init(&h);
+
+    heap_insert(&h, 1);
+    heap_insert(&h, 2);
+    heap_insert(&h, 3);
+    heap_insert(&h, 4);
+    heap_insert(&h, 5);
+    heap_insert(&h, 6);
+
+    ck_assert_int_eq(1, heap_minimum(&h));
+}
+END_TEST
+
 int
 main(void)
 {
@@ -65,6 +81,7 @@ main(void)
     tcase_add_test(testcase, test_heap_with_two_increasing_values);
     tcase_add_test(testcase, test_heap_with_two_decreasing_values);
     tcase_add_test(testcase, test_heap_with_many_decreasing_values);
+    tcase_add_test(testcase, test_heap_with_many_increasing_values);
 
     srunner_run_all(runner, CK_ENV);
 }
